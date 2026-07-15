5bd4960fd0622bfe446f56087b07466cd5ee8fe3
sriharsha256 <boligarlasriharsha@gmail.com>
2026-07-15 12:06:45 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        vector<int> v(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(int i=0;i<v.size();i++)
+        {
+            if(v[i]!=0) return false;
+        }
+        return true;
+    }
+};
